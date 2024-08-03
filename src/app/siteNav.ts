2da5df1c582923592ef0e1e2ac95f943c2e433c6
indra putra <notindrapratama@gmail.com>
2024-08-03 23:03:07 +0700

    update

---
+++ src/app/siteNav.ts
@@ -0,0 +1,13 @@
+const siteNav = [
+    {
+        title: 'beranda',
+        href: '/',
+        subMenu: [],
+    },
+    {
+        title: 'berita',
+        href: '/berita'
+    }
+]
+
+export default siteNav;
